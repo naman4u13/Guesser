b771645a9b2c84ac7a466965f15710663397981a
Naman Agarwal <naman4u13@gmail.com>
2018-08-20 08:53:47 +0530

    first commit

---
+++ app/src/main/java/com/example/hp/guesser/MainActivity.java
@@ -0,0 +1,45 @@
+package com.example.hp.guesser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    EditText eview;
+    TextView display;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        eview = (EditText)findViewById(R.id.eview);
+        display = (TextView)findViewById(R.id.display);
+    }
+    void  guess(View view)
+    {
+        int number;
+        try {
+            number = Integer.parseInt(eview.getText().toString());
+
+            if (number < 50)
+            {
+                display.setText("Guess a bigger number", TextView.BufferType.EDITABLE);
+            } else if (number > 50) {
+                display.setText("Guess a smaller number", TextView.BufferType.EDITABLE);
+            } else {
+                display.setText("CONGRATULATIONS !! YOU WON", TextView.BufferType.EDITABLE);
+            }
+        }
+        catch(NumberFormatException e)
+        {
+
+            display.setText("Wrong Input", TextView.BufferType.EDITABLE);
+        }
+    }
+
+     void retry(View view)
+    {
+        eview.setText("");
+    }
+}
